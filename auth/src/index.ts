61a0e09ea72a575487471343d3c14f40b8545bde
ShreyaPatel20063 <sap20063@gmail.com>
2023-08-21 21:48:20 +0530

    get request added

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { json } from "body-parser";
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+const app = express();
+app.use(json());
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+});
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,9 @@
+import express, {Request, Response} from 'express';
+
+const router = express.Router();
+
+router.get("/api/users/signin", (req: Request, res: Response) => {
+  res.send("hii there SignIn..!");
+});
+
+export { router as signinRouter };
