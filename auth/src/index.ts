961c4d6ef5fe647ce4ba2b3478e1856556e13ddb
ShreyaPatel20063 <sap20063@gmail.com>
2023-08-21 22:22:07 +0530

    formatting added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { json } from "body-parser";
-import { currentuserRouter } from './routes/current-user' ;
-import { signinRouter } from './routes/signin' ;
-import { signoutRouter } from './routes/signout' ;
-import { signupRouter } from './routes/signup' ;
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,23 +3,25 @@ import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signout", 
-[
-    body('email').isEmail().withMessage('must be a valid email'),
-    body('password').trim().isLength({min:4, max:20}).withMessage('must be between 4 and 20 characters')
-],
-(req: Request, res: Response) => {
+router.post(
+  "/api/users/signout",
+  [
+    body("email").isEmail().withMessage("must be a valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
     const err = validationResult(req);
 
-    if(!err.isEmpty()){
-        return res.status(400).send(err.array());
-    }  
+    if (!err.isEmpty()) {
+      return res.status(400).send(err.array());
+    }
 
     const { email, password } = req.body;
     res.json({ email, password });
-}
-
-
+  }
 );
 
 export { router as signoutRouter };
