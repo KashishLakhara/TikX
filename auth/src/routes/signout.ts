27ec793d74f639af54f019fe72d4d4d505786f9f
ShreyaPatel20063 <sap20063@gmail.com>
2023-08-21 22:10:48 +0530

    post method added

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", (req: Request, res: Response) => {
-  res.send("POST request ");
-});
+router.post("/api/users/signout", 
+[
+    body('email').isEmail().withMessage('must be a valid email'),
+    body('password').trim().isLength({min:4, max:20}).withMessage('must be between 4 and 20 characters')
+],
+(req: Request, res: Response) => {
+    const err = validationResult(req);
+
+    if(!err.isEmpty()){
+        return res.status(400).send(err.array());
+    }  
+
+    const { email, password } = req.body;
+    res.json({ email, password });
+}
+
+
+);
 
 export { router as signoutRouter };
