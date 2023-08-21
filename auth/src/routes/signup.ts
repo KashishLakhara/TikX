7b45af9614ae5a8b33f14c501875911342d4d2ff
ShreyaPatel20063 <sap20063@gmail.com>
2023-08-21 22:10:48 +0530

    post method added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,7 @@ import express, {Request, Response} from 'express';
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
+router.get("/api/users/signup", (req: Request, res: Response) => {
   res.send("hii there SignUp..!");
 });
 
